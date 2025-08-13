f370067f8a88a9ee5084ee17bb1838f92dc8778a
1suleyman <110779287+1suleyman@users.noreply.github.com>
2025-08-13 16:46:55 +0100

    Create versions.tf

---
+++ Content/🌍 Module 1 — IAM Setup + Global Project Configuration/versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  # Backend values are supplied at init time via -backend-config
+  backend "s3" {}
+#   for example 
+# cd Module_1
+# terraform init -backend-config=backend.hcl -reconfigure
+# terraform apply
+}
